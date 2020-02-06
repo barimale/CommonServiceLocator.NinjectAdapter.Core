6235a74807be411309cbf7eb766a2ce9e753930f
barimale <mateusz.wolnica@gmail.com>
2020-02-06 15:20:48 +0100

    Initial commit.

---
+++ CommonServiceLocator.NinjectAdapter.Extension/NinjectServiceLocator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Ninject;
+using System;
+using System.Collections.Generic;
+
+namespace CommonServiceLocator.NinjectAdapter.Extension
+{
+    public class NinjectServiceLocator : ServiceLocatorImplBase
+    {
+        public IKernel Kernel { get; private set; }
+
+        public NinjectServiceLocator(IKernel kernel)
+        {
+            Kernel = kernel;
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            if (key == null)
+            {
+                return Kernel.Get(serviceType);
+            }
+            return Kernel.Get(serviceType, key);
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return Kernel.GetAll(serviceType);
+        }
+    }
+}
---
+++ UT.CommonServiceLocator.NinjectAdapter.Extension.NET47/As_a_developer/When_nuget_is_in_use.cs
@@ -0,0 +1,22 @@
+﻿using CommonServiceLocator.NinjectAdapter.Extension;
+using Ninject;
+using NUnit.Framework;
+
+namespace UT.CQRSlite.Ninject.Binding.NET47
+{
+    public class When_nuget_is_in_use
+    {
+        [Test]
+        public void I_d_like_to_have_bindings_loaded_correctly()
+        {
+            //given
+            StandardKernel kernel = new StandardKernel();
+
+            //when
+            NinjectServiceLocator locator = new NinjectServiceLocator(kernel);
+
+            //then
+            Assert.IsNotNull(locator);
+        }
+    }
+}
